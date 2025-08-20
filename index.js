944a986de492621a707fef1eb1ec57755ac5c4c3
Namrata Kumari <namrata.kumari@argusoft.com>
2025-08-20 15:51:09 +0530

    new folders added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {createServer} = require('node:http');
 const {join} = require('node:path');
 const {Server} = require('socket.io');
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server,{connectionStateRecovery:{}});
 
 // by default express does not serve static files so we need to explicitly tell it to serve static files like html and css
 app.use(express.static('public'));
@@ -22,7 +24,8 @@ io.on('connection',(socket)=>{
     });
 
     socket.on('chat message',(msg)=>{
-        io.emit('chat message',msg);
+        io.emit('chat message',msg);//sending message to everyone including the sender
+        // socket.broadcast.emit('chat message',msg); // sending msg to everyone except the sender
     });
 });
 
